5b5b81d3fa5568b10ed812692e41ab33323040ed
Travis Prosser <travisprosser@gmail.com>
2024-11-16 12:53:52 -0400

    Remove newtypes for now and add more route handlers

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,25 @@
 mod models;
 mod routes;
 
-use fake::{Fake, Faker};
-use models::{LogRecord, Person, Vehicle, VehicleMake, VehicleModel, VehicleYear};
+use std::env;
 
 use axum::{routing::get, Router};
-use routes::{get_logs, get_vehicle, get_vehicles};
+use routes::{log_records, vehicles};
+
 #[tokio::main]
 async fn main() {
-    let person = Faker.fake::<Person>();
-
-    let mut civic = Vehicle::new(
-        VehicleMake("Honda".to_owned()),
-        VehicleModel("Civic".to_owned()),
-        VehicleYear(2020),
-        person,
-        models::OdometerUnit::Metric,
-    );
-
-    let record1 = Faker.fake::<LogRecord>();
-    let record2 = Faker.fake::<LogRecord>();
-
-    civic.add_record(record1);
-    civic.add_record(record2);
-
-    dbg!(&civic);
-
-    println!("Civic JSON: {}", serde_json::to_string(&civic).unwrap());
-    println!("Current odometer: {}", civic.get_current_odo());
-
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/logs/:vehicle_id", get(get_logs))
-        .route("/vehicle/:vehicle_id", get(get_vehicle))
-        .route("/vehicles", get(get_vehicles));
+        .nest("/vehicles", vehicles::build_router())
+        .nest("/logs", log_records::build_router());
+
+    let port = env::var("PORT").unwrap_or("3000".to_owned());
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("Running on: localhost:3000");
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
+        .await
+        .unwrap();
+    println!("Running on: localhost:{port}");
     axum::serve(listener, app).await.unwrap();
 }
--- src/models/log_record.rs
+++ src/models/log_record.rs
@@ -1,36 +1,131 @@
-use std::fmt::Display;
-
 use chrono::{DateTime, Utc};
 use fake::Dummy;
-use serde::Serialize;
-
-#[derive(Debug, Serialize, Dummy, Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]
-pub struct OdometerKms(#[dummy(faker = "0..500000")] pub u32);
-
-impl Display for OdometerKms {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-#[derive(Debug, Serialize, Dummy)]
-pub struct FuelAmountLitres(#[dummy(faker = "5.0..120.0")] pub f32);
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[allow(dead_code)]
-#[derive(Debug, Serialize, Dummy)]
+#[derive(Debug, Serialize, PartialEq, Deserialize, Dummy, Clone)]
+#[serde(rename_all = "snake_case")]
+#[serde(tag = "log_type")]
 pub enum LogType {
-    FuelUp { amount: FuelAmountLitres },
+    FuelUp {
+        #[dummy(faker = "5.0..120.0")]
+        fuel_amount: f32,
+    },
     // TireRotation,
     // TireChange,
     // OilChange,
     // Repair,
-    // WiperBlades,
+    // WiperBladeReplacement,
     // BatteryReplacement,
+    // Brakes
 }
 
-#[derive(Debug, Serialize, Dummy)]
+#[derive(Debug, Serialize, Dummy, PartialEq, Clone)]
 pub struct LogRecord {
+    pub id: Uuid,
     pub date: DateTime<Utc>,
+    #[serde(flatten)]
     pub log_type: LogType,
-    pub odometer: OdometerKms,
+    #[dummy(faker = "0..500000")]
+    pub odometer: u32,
+}
+
+#[derive(Debug, Deserialize, Dummy, PartialEq)]
+pub struct LogRecordInput {
+    pub date: Option<DateTime<Utc>>,
+    #[serde(flatten)]
+    pub log_type: LogType,
+    #[dummy(faker = "0..500000")]
+    pub odometer: u32,
+}
+
+#[cfg(test)]
+mod log_record_tests {
+    use fake::{Fake, Faker};
+    use serde_json::json;
+
+    use crate::models::{LogRecordInput, LogType};
+
+    use super::{DateTime, LogRecord, Utc};
+
+    #[test]
+    fn log_record_serializes_fuel_up() {
+        // Arrange
+        let sample_record = LogRecord {
+            log_type: LogType::FuelUp {
+                fuel_amount: Faker.fake(),
+            },
+            ..Faker.fake()
+        };
+
+        let LogType::FuelUp { fuel_amount } = sample_record.log_type;
+
+        #[allow(irrefutable_let_patterns)]
+        let expected = json!({
+            "id": sample_record.id,
+            "date": format!("{:?}", sample_record.date),
+            "log_type": "fuel_up",
+            "fuel_amount": fuel_amount,
+            "odometer": sample_record.odometer,
+        });
+
+        // Act
+        let serialized = serde_json::to_value(&sample_record).unwrap();
+
+        // Assert
+        assert_eq!(serialized, expected);
+    }
+
+    #[test]
+    fn log_record_input_deserializes_fuel_up_with_date() {
+        // Arrange
+        let date = Faker.fake::<Option<DateTime<Utc>>>();
+        let fuel_amount = Faker.fake::<f32>();
+        let odometer = Faker.fake::<u32>();
+
+        let json = json!({
+            "date": if let Some(date) = date {Some(format!("{:?}", date))} else {None},
+            "log_type": "fuel_up",
+            "fuel_amount": fuel_amount,
+            "odometer": odometer,
+        });
+
+        let expected = LogRecordInput {
+            log_type: LogType::FuelUp { fuel_amount },
+            date: date,
+            odometer,
+        };
+
+        // Act
+        let deserialized = serde_json::from_value::<LogRecordInput>(json).unwrap();
+
+        // Assert
+        assert_eq!(deserialized, expected);
+    }
+
+    #[test]
+    fn log_record_input_deserializes_fuel_up_with_no_date() {
+        // Arrange
+        let fuel_amount = Faker.fake::<f32>();
+        let odometer = Faker.fake::<u32>();
+
+        let json = json!({
+            "log_type": "fuel_up",
+            "fuel_amount": fuel_amount,
+            "odometer": odometer,
+        });
+
+        let expected = LogRecordInput {
+            log_type: LogType::FuelUp { fuel_amount },
+            date: None,
+            odometer,
+        };
+
+        // Act
+        let deserialized = serde_json::from_value::<LogRecordInput>(json).unwrap();
+
+        // Assert
+        assert_eq!(deserialized, expected);
+    }
 }
