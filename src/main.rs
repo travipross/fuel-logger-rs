6bfde4dc033a56506704172c9d2603667dd451b7
travipross <travisprosser@gmail.com>
2024-11-22 00:19:13 -0400

    feat: set up tracing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use anyhow::Context;
 use axum::serve;
 use clap::Parser;
-use fuel_logger_rs::{build_router, configuration::read_config};
+use fuel_logger_rs::{
+    build_router,
+    configuration::{read_config, Configuration, LogFormat},
+};
 use sqlx::postgres::PgPoolOptions;
 use std::time::Duration;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 #[derive(clap::Parser, Debug)]
 #[command(version)]
@@ -11,13 +15,12 @@ use std::time::Duration;
 ///
 /// Run a webserver hosting a vehicle log API.
 struct Cli {
-    /// Print a sample configuration to stdout
+    /// Print a sample configuration to stdout and then exit
     #[arg(long)]
     init_config: bool,
 }
 
-async fn run() -> anyhow::Result<()> {
-    let config = read_config().context("failed to load configuration")?;
+async fn run(config: Configuration) -> anyhow::Result<()> {
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .acquire_timeout(Duration::from_secs(5))
@@ -35,20 +38,48 @@ async fn run() -> anyhow::Result<()> {
     let listener = tokio::net::TcpListener::bind(&addr)
         .await
         .context("failed to create TCP listener")?;
-    println!("Running on: {addr}");
+    tracing::info!("serving application on: {addr}");
     serve(listener, app)
         .await
         .context("failed to serve axum app")?;
     Ok(())
 }
 
+#[tracing::instrument]
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // Load config
+    let config = read_config().context("failed to load configuration")?;
+
+    // Set tracing filter for log level
+    let filter = EnvFilter::default()
+        .add_directive(config.log.level.to_string().parse()?)
+        .add_directive("sqlx=info".parse()?);
+
+    // Set tracing layers for log format
+    // Pattern from from https://github.com/tokio-rs/tracing/blob/master/examples/examples/toggle-subscribers.rs
+    let (pretty, compact, full) = match config.log.format {
+        LogFormat::Pretty => (Some(tracing_subscriber::fmt::layer().pretty()), None, None),
+        LogFormat::Compact => (None, Some(tracing_subscriber::fmt::layer().compact()), None),
+        LogFormat::Full => (None, None, Some(tracing_subscriber::fmt::layer())),
+    };
+
+    // Initialize tracing subscriber registry with filter and format layers
+    tracing_subscriber::registry()
+        .with(filter)
+        .with(pretty)
+        .with(compact)
+        .with(full)
+        .try_init()
+        .context("registering tracing subscriber")?;
+
+    // Handle CLI command
     let cli_args = Cli::parse();
     if cli_args.init_config {
         print!(include_str!("../dev/example-config.yml"));
         Ok(())
     } else {
-        run().await
+        tracing::debug!("running application");
+        run(config).await
     }
 }
